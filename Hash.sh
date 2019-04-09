bb00512bfd569168899b802f9fc5e5f631365e04
a9210 <test@test.com>
2019-04-10 00:40:04 +0900

    fix put

--- Hash.sh
+++ Hash.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 function put {
-        EVAL_STR="${1}=${2}"
-        eval ${EVAL_STR}
+    EVAL_STR="${1}=\"${2}\""
+    eval ${EVAL_STR}
 }
 function get {
-        EVAL_STR="echo \$\{${1}\}"
-        eval echo '${'${1}'}'
+    EVAL_STR="echo \$\{${1}\}"
+    eval echo '${'${1}'}'
 }
