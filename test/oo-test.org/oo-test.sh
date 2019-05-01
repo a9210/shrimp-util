bb68aea54d7971ad1456e66200e0bda90b2c0868
a9210 <test@test.com>
2019-05-01 23:32:20 +0900

    remove debug code #14

--- test/oo-test.org/oo-test.sh
+++ test/oo-test.org/oo-test.sh
@@ -4,8 +4,9 @@ source include.sh
 
 shrimp install shrimp-oo https://github.com/a9210/shrimp-oo develop
 cp ../../*.sh ./packages/shrimp-util
-echo "test1"
-bash -x ./Hash.sh  || TEST_RESULT=1
-echo "test2"
+
+#bash -x ./Hash.sh  || TEST_RESULT=1
+./Hash.sh  || TEST_RESULT=1
+
 
 exit ${TEST_RESULT}
