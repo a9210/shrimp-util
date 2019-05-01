6732313dc57cd03536b157e73b3772c006162402
a9210 <test@test.com>
2019-05-01 10:44:52 +0900

    add oo-test #14

---
+++ test/oo-test.org/oo-test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+TEST_RESULT=0
+source include.sh
+
+shrimp install shrimp-oo https://github.com/a9210/shrimp-oo develop
+cp ../../*.sh ./packages/shrimp-util
+
+./Hash.sh  || TEST_RESULT=1
+
+exit ${TEST_RESULT}
